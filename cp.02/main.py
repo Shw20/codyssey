62f01db456e95a3ad8017c7c36745a6bb93d0f97
Shw20 <alkongi03@gmail.com>
2026-03-17 20:13:21 +0900

    add : 마크다운 파일 생성

---
+++ cp.02/main.py
@@ -0,0 +1,30 @@
+def file_log(file_name):
+    try:
+        with open(file_name, 'r', encoding='utf-8')as f:
+            lines = f.readlines()
+        errors = [line.strip() for line in lines if "ERROR" in line]
+        return errors
+    except FileNotFoundError:
+        return "파일을 찾을 수 없습니다."
+
+def save_report(errors):
+    with open("report.md", "w", encoding="utf-8") as f:
+        f.write("# 🛡️ 컴퓨터 보수 결과 보고서\n\n")
+        f.write("## 🔍 파악된 사고 원인\n")
+        if isinstance(errors, list) and errors:
+            for err in errors:
+                f.write(f"- {err}\n")
+        else:
+            f.write("문제 없음\n")
+    print("보고서가 생성되었습니다")
+    
+# --- 실행부 ---
+# 테스트를 위해 임시 로그 파일 생성
+with open("logs.txt", "w", encoding="utf-8") as f:
+    f.write("System Start\nERROR: CPU Fan Failure\nERROR: Memory Overload\nSystem End")
+ㄴ
+# 로그 분석 후 마크다운 저장
+found_errors = file_log("logs.txt")
+save_report(found_errors)
+
+# 
---
+++ report.md
@@ -0,0 +1,5 @@
+# 🛡️ 컴퓨터 보수 결과 보고서
+
+## 🔍 파악된 사고 원인
+- ERROR: CPU Fan Failure
+- ERROR: Memory Overload
